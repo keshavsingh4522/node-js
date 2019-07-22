96c926b35b30dcf261c3c3dfdaeeb7e283e664bd
Keshav Singh <keshavsingh4522@gmail.com>
2019-07-22 22:25:59 +0530

    Create 1st_method.js

---
+++ basic/export_require/1st_method.js
@@ -0,0 +1,10 @@
+// 1st method
+exports.hello=function(){
+	return 'Hello user,u have successfully used local module';
+}
+exports.ola=function(){
+	return 'your ola ride transaction is here';
+}
+exports.github=function(){
+	return 'your github document is here';
+}
