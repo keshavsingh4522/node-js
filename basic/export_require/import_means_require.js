4a6ce584decd00551f1ac689a0c7ed92ce267cf6
Keshav Singh <keshavsingh4522@gmail.com>
2019-07-22 22:26:57 +0530

    Create import_means_require.js

---
+++ basic/export_require/import_means_require.js
@@ -0,0 +1,7 @@
+var h=require('./export.js');
+var f=require('./export2.js');
+console.log(h.hello());
+console.log(h.ola());
+console.log(h.github());
+console.log(f.facebook());
+console.log(f.instagram());
